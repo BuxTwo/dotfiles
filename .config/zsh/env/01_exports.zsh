fb09690ec07167b27e2989a1b9bc0dbc36cbd6e6
BuxTwo <arseniyarkhipov05@gmail.com>
2026-04-25 11:45:36 +0300

    Some fixes

--- .config/zsh/env/01_exports.zsh
+++ .config/zsh/env/01_exports.zsh
@@ -25,7 +25,6 @@ export PAGER=less
 export LESS="--RAW-CONTROL-CHARS --ignore-case --LONG-PROMPT --use-color --tabs=4 --mouse"
 export READNULLCMD=$PAGER
 
-export TERM="xterm-256color"
 export GNUPGHOME="$XDG_CONFIG_HOME/gnupg"
 export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc-2.0"
 
@@ -42,10 +41,5 @@ if (( ${+commands[bat]} )); then
 fi
 
 if (( ${+commands[fzf]} )); then
-    export FZF_DEFAULT_OPTS=" \
-        --color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
-        --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
-        --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
-        --color=selected-bg:#45475A \
-        --color=border:#6C7086,label:#CDD6F4"
+    export FZF_DEFAULT_OPTS="--color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 --color=selected-bg:#45475A --color=border:#6C7086,label:#CDD6F4"
 fi
